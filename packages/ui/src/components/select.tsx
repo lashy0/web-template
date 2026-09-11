c4045e56bd6c2b07b44321cdc4a14d5fa09045a4
Daniil Alekseev <lashy.danya.work@gmail.com>
2026-09-11 19:03:00 +0300

    fix(ui): truncate long select item labels

--- packages/ui/src/components/select.tsx
+++ packages/ui/src/components/select.tsx
@@ -116,7 +116,7 @@ function SelectItem({ className, children, ...props }: SelectPrimitive.Item.Prop
       )}
       {...props}
     >
-      <SelectPrimitive.ItemText className="flex flex-1 shrink-0 gap-2 whitespace-nowrap">
+      <SelectPrimitive.ItemText className="flex min-w-0 flex-1 gap-2 truncate">
         {children}
       </SelectPrimitive.ItemText>
       <SelectPrimitive.ItemIndicator
